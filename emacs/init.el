66dbfc85c389dc8ea4ad79220bae9db37fef5e98
Carl Lieberman <liebermancarl@gmail.com>
2015-07-06 20:22:29 -0400

    * emacs/init.el: Initial Org-Mode commit

--- emacs/init.el
+++ emacs/init.el
@@ -122,3 +122,16 @@
 (emms-all)
 (emms-default-players)
 (setq emms-source-file-default-directory "~/Music/")
+     (require 'emms-mode-line)
+     (emms-mode-line 0)
+     (require 'emms-playing-time)
+     (emms-playing-time 0)
+
+;;;;;;;;;;;;;;
+;; Org-Mode ;;
+;;;;;;;;;;;;;;
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode)) ; not needed since Emacs 22.2
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
